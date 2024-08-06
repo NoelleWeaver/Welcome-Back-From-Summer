11e639abb968f4ac1bba919405770d38959de3b3
NoelleWeaver <nweave474@west-mec.org>
2024-08-06 15:19:53 -0700

    a

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const nav = document.querySelector('nav');
 window.addEventListener('scroll', () => {
-    if(window.scrollY >= 200){
+    if(window.scrollY >= 765){
         nav.classList.add('white')
     }
     else{
